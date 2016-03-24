623b1e5ce431595d6062426953969b2c249b46ea
Justin Pierce <brkfstmnchr@gmail.com>
2016-03-24 15:48:05 -0700

    removed formTemplate designer file; template window now invokes data update each time the window is shown (instead of just the first time)

--- RiotPls/Forms/formTemplate.cs
+++ RiotPls/Forms/formTemplate.cs
@@ -4,10 +4,17 @@ using System.Windows.Forms;
 
 namespace RiotPls.Forms
 {
-    public partial class formTemplate : Form
+    public class formTemplate : Form
     {
-        #region Instance Members
-        protected Point last_mouse = Point.Empty;
+        #region Instance Members  
+        #region Controls               
+        private System.ComponentModel.IContainer components = null;
+        protected System.Windows.Forms.Button btnClose;
+        protected System.Windows.Forms.Button btnSettings;
+        protected System.ComponentModel.BackgroundWorker workerUpdateData;
+        protected System.Windows.Forms.PictureBox picLoading;
+        #endregion
+        private Point last_mouse = Point.Empty;
         #endregion
         #region Instance Properties  
         public bool ChildWindow
@@ -22,9 +29,93 @@ namespace RiotPls.Forms
         } = false;
         #endregion
         #region Instance Methods
-        public formTemplate()
+        protected formTemplate()
         {
             this.InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formTemplate));
+            this.btnClose = new System.Windows.Forms.Button();
+            this.workerUpdateData = new System.ComponentModel.BackgroundWorker();
+            this.picLoading = new System.Windows.Forms.PictureBox();
+            this.btnSettings = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picLoading)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.btnClose.Location = new System.Drawing.Point(302, 9);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(20, 20);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "x";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnClose_MouseDown);
+            // 
+            // workerUpdateData
+            // 
+            this.workerUpdateData.DoWork += new System.ComponentModel.DoWorkEventHandler(this.workerUpdateData_DoWork);
+            this.workerUpdateData.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.workerUpdateData_RunWorkerCompleted);
+            // 
+            // picLoading
+            // 
+            this.picLoading.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picLoading.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.picLoading.Image = global::RiotPls.Properties.Resources.Loading;
+            this.picLoading.InitialImage = null;
+            this.picLoading.Location = new System.Drawing.Point(101, 77);
+            this.picLoading.Name = "picLoading";
+            this.picLoading.Size = new System.Drawing.Size(128, 128);
+            this.picLoading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picLoading.TabIndex = 7;
+            this.picLoading.TabStop = false;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackgroundImage = global::RiotPls.Properties.Resources.Gears;
+            this.btnSettings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSettings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Location = new System.Drawing.Point(10, 10);
+            this.btnSettings.Margin = new System.Windows.Forms.Padding(1);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(16, 16);
+            this.btnSettings.TabIndex = 9;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
+            // formTemplate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(331, 282);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.picLoading);
+            this.Controls.Add(this.btnClose);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "formTemplate";
+            this.Text = "formTemplate";
+            this.VisibleChanged += new System.EventHandler(this.formTemplate_VisibleChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.formTemplate_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.formTemplate_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.picLoading)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         protected void LoadWindowSettings()
@@ -38,7 +129,7 @@ namespace RiotPls.Forms
             return;
         }
 
-        public void UpdateData()
+        protected void UpdateData()
         {
             this.picLoading.Visible = this.ShowLoading;
             this.picLoading.BringToFront();
@@ -48,11 +139,6 @@ namespace RiotPls.Forms
 
         #endregion
         #region Event Methods
-        private void formTemplate_Load(object sender, System.EventArgs e)
-        {
-            this.UpdateData();
-        }
-
         protected virtual void btnSettings_Click(object sender, EventArgs e)
         {
         }
@@ -84,7 +170,10 @@ namespace RiotPls.Forms
         private void formTemplate_VisibleChanged(object sender, EventArgs e)
         {
             if (this.Visible)
+            {
                 Tools.GeneralSettings.LoadWindowSettings(this);
+                this.UpdateData();
+            }
             else
                 Tools.GeneralSettings.SaveWindowSettings(this);
             return;
@@ -97,6 +186,14 @@ namespace RiotPls.Forms
         {
 
         }
-        #endregion
+        #endregion  
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
